3866a7e4c7f6e1baaecb8f213c9c9727123b15e3
thebaptiste <jean-baptiste.veslin@meteo.fr>
2020-04-17 08:58:22 +0000

    build: fix comment

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #LDFLAGS=-Wl,-O2 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,--disable-new-dtags -Wl,--gc-sections -Wl,-rpath,$PREFIX/lib -Wl,-rpath-link,$PREFIX/lib -L$PREFIX/lib
-# For some reason, conda compiler default LDFLAGS (above) doesn't work with log_proxy. So we set LDFLAGS to nothing and export the variable in meta.yaml
+# For some reason, conda compiler default LDFLAGS (above) doesn't work with log_proxy. So we set LDFLAGS to nothing.
 export LDFLAGS=
 make all
 make install
