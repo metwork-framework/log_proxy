cd8c515338cca4e217a83b568ad3e91b6be72055
Fabien MARTY <fabien.marty@gmail.com>
2020-12-05 10:35:43 +0100

    feat: add a chmod/chown/chgrp option (#22)
    Close: #21

--- src/out.h
+++ src/out.h
@@ -3,7 +3,7 @@
 
 #include <glib.h>
 
-void init_output_channel(const gchar *log_file, gboolean use_locks, gboolean force_control_file);
+void init_output_channel(const gchar *log_file, gboolean use_locks, gboolean force_control_file, const gchar *chmod_str, const gchar *chown_str, const gchar *chgrp_str);
 void destroy_output_channel();
 gboolean write_output_channel(GString *buffer);
 glong get_output_channel_age();
