bf3ffced7f3661b06582721c90e3bd38f015f91c
Mike Kazantsev <mk.fraggod@gmail.com>
2024-02-09 17:13:27 +0500

    perf: write out log line with time-prefix as one string to avoid holding locks (#40)
    Earlier implementation in #35 can loop while holding the lock, which is not a good idea, and composing prefix+line into one GString is simpler anyway.
    Using glib datetime and string formatting also allows to simplify implementation.

--- src/util.h
+++ src/util.h
@@ -7,7 +7,6 @@
 glong get_file_size(const gchar *file_path);
 glong get_current_timestamp();
 gchar *compute_strftime_suffix(const gchar *str, const gchar *strftime_suffix);
-gchar *compute_timestamp_prefix(const gchar *strftime_prefix);
 gchar *get_unique_hexa_identifier();
 glong get_file_inode(const gchar *file_path);
 glong get_fd_inode(int fd);
